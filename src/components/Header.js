0fcd4e673f94b5eb55711fc07dbe6ff09690c7cd
dariyaamangeldi <amangeldi.dariya@narxoz.kz>
2024-02-23 01:19:56 +0600

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Header = () => {
+    return (
+        <div className="header">
+            <h1> List by Dariya</h1>
+        </div>
+    )
+}
+export default Header;
